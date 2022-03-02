74e89db601397489ba203170a12d6f5cf5581ffe
teaganconnon <teagan@ucsb.edu>
2022-03-02 13:32:12 -0800

    tc-added level selector to basic course search form copied from s21 form

---
+++ frontend/src/main/components/CourseSearch/CourseSearchForm.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+import { Form, Button, Container, Row, Col } from "react-bootstrap";
+import { allTheSubjects } from "fixtures/subjectFixtures.js"
+import { quarterRange } from "main/utils/quarterUtils.js"
+import LevelSelector from "main/components/CourseSearch/LevelSelector";
+
+const BasicCourseSearchForm = ({ setCourseJSON, fetchJSON }) => {
+	const quarters = quarterRange("20084", "20222");
+	const levels = [["L","Undergrad-Lower"], 
+					["S","Undergrad-Upper Division"], 
+					["U","Undergrad-All"], 
+					["G","Graduate"]];
+
+    const localSubject = localStorage.getItem("BasicSearch.Subject");
+    const localQuarter = localStorage.getItem("BasicSearch.Quarter");
+	const localLevel = localStorage.getItem("BasicSearch.CourseLevel");
+	
+	const firstDepartment = allTheSubjects[0].subjectCode;
+	const [quarter, setQuarter] = useState(localQuarter || quarters[0].yyyyq);
+	const [subject, setSubject] = useState(localSubject || firstDepartment);
+    const [level, setLevel] = useState(localLevel || "U");
+
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		fetchJSON(event, { quarter, subject, level }).then((courseJSON) => {
+			if (courseJSON.total === 0) {
+			}
+			setCourseJSON(courseJSON);
+		});
+	};
+
+	const handleLevelOnChange = (level) => {
+        localStorage.setItem("BasicSearch.CourseLevel", level);
+		setLevel(level);
+	};
+
+	return (
+		<Form onSubmit={handleSubmit}>
+			<Container>
+				<Row>
+					<Col md = "auto"><LevelSelector
+						levels={levels}
+						level={level}
+						setLevel={handleLevelOnChange}
+						controlId={"BasicSearch.CourseLevel"}
+						label={"Course Level"}
+					/></Col>
+				</Row>
+			</Container>
+			<Button variant="primary" type="submit">
+				Submit
+			</Button>
+		</Form>
+	);
+};
+
+export default BasicCourseSearchForm;
--- frontend/src/main/pages/CourseSearchPage.js
+++ frontend/src/main/pages/CourseSearchPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import CourseSearchForm from "main/components/CourseSearch/CourseSearchForm";
 
 import { useBackend } from "main/utils/useBackend";
 const CourseSearchPage = () => {
@@ -7,7 +8,7 @@ const CourseSearchPage = () => {
     return (
         <BasicLayout>
             <h2>Course Search</h2>
-            <p>This is a stub page</p>
+            <CourseSearchForm/>
         </BasicLayout>
     );
 };
