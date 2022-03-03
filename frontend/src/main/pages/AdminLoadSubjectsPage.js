98eebac9e46cb5109cd67975bcb59038a6ae47a9
Justin Lai <justinlai@ResNet-28-169.resnet.ucsb.edu>
2022-03-02 22:26:20 -0800

    jl - Deleted unnecessary lines in AdminLoadSubjectsPage.js, now giving 100% in stryker testing

--- frontend/src/main/pages/AdminLoadSubjectsPage.js
+++ frontend/src/main/pages/AdminLoadSubjectsPage.js
@@ -5,14 +5,7 @@ import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 import { useBackend } from "main/utils/useBackend";
 const AdminLoadSubjectsPage = () => {
 
-    const { data: users, error: _error, status: _status } =
-        useBackend(
-            // Stryker disable next-line all : don't test internal caching of React Query
-            ["/api/admin/loadSubjects"],
-            { method: "GET", url: "/api/admin/loadSubjects" },
-            []
-        );
-
+    
     return (
         <BasicLayout>
             <h2>LoadSubjects</h2>
