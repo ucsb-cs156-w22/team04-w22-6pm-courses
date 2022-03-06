7cc167153cca85064e84d951d8fe2d2b4fff00e5
akelkordy <akelkordy@ucsb.edu>
2022-03-06 08:51:14 -0800

    Added UCSB Subjects Table, alongside its tests and storybook component

---
+++ frontend/src/fixtures/ucsbSubjectsFixtures.js
@@ -0,0 +1,44 @@
+
+const ucsbSubjectsFixtures = {
+    oneSubject: {
+        "id" : "randomId",
+        "subjectCode": "randomSubjectCode",
+        "subjectTranslation" : "randomTranslation",
+        "deptCode" : "randomDeptCode",
+        "collegeCode" : "randomCollegeCode",
+        "relatedDeptCode" :"randomRelatedDeptCode",
+        "inative" : false
+    },
+    threeSubjects: [
+        {
+            "id" : "1",
+            "subjectCode": "0001",
+            "subjectTranslation" : "randomTranslation1",
+            "deptCode" : "randomDeptCode1",
+            "collegeCode" : "randomCollegeCode1",
+            "relatedDeptCode" :"randomRelatedDeptCode1",
+            "inative" : false
+        },
+        {
+            "id" : "2",
+            "subjectCode": "1003",
+            "subjectTranslation" : "randomTranslation2",
+            "deptCode" : "randomDeptCode2",
+            "collegeCode" : "randomCollegeCode2",
+            "relatedDeptCode" :"randomRelatedDeptCode2",
+            "inative" : false
+        },
+        {
+            "id" : "3",
+            "subjectCode": "2016",
+            "subjectTranslation" : "randomTranslation3",
+            "deptCode" : "randomDeptCode3",
+            "collegeCode" : "randomCollegeCode3",
+            "relatedDeptCode" :"randomRelatedDeptCode3",
+            "inative" : false
+        }
+    ]
+};
+
+
+export { ucsbSubjectsFixtures };
