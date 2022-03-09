c7c8e5ad12edc166d483d2bf625cbe0058d078b9
ZeyuanLu <zeyuan_lu@ucsb,edu>
2022-03-09 00:53:42 -0800

    add table component for personalSchedules with tests

---
+++ .stryker-tmp/sandbox3547400/frontend/src/App.js
@@ -0,0 +1,39 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "main/pages/HomePage";
+import ProfilePage from "main/pages/ProfilePage";
+import AdminUsersPage from "main/pages/AdminUsersPage";
+import PersonalSchedulesIndexPage from "main/pages/PersonalSchedules/PersonalSchedulesIndexPage";
+import PersonalSchedulesCreatePage from "main/pages/PersonalSchedules/PersonalSchedulesCreatePage";
+
+import { hasRole, useCurrentUser } from "main/utils/currentUser";
+
+import "bootstrap/dist/css/bootstrap.css";
+
+
+function App() {
+
+  const { data: currentUser } = useCurrentUser();
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route exact path="/profile" element={<ProfilePage />} />
+        {
+          hasRole(currentUser, "ROLE_ADMIN") && <Route exact path="/admin/users" element={<AdminUsersPage />} />
+        }
+
+        {
+          hasRole(currentUser, "ROLE_USER") && (
+            <>
+              <Route exact path="/personalschedules/list" element={<PersonalSchedulesIndexPage />} />
+              <Route exact path="/personalschedules/create" element={<PersonalSchedulesCreatePage />} />
+            </>
+          )
+        }
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ .stryker-tmp/sandbox3547400/frontend/src/tests/components/Courses/CoursesTable.test.js
@@ -0,0 +1,83 @@
+import { render, waitFor } from "@testing-library/react";
+import CoursesTable from "main/components/Courses/CoursesTable";
+import {coursesFixtures} from 'fixtures/coursesFixtures'
+
+describe("CoursesTable tests", () => {
+
+    test("renders an empty table without crashing", () => {
+        render(
+            <CoursesTable courses={[]} />
+        );
+    });
+
+    test("renders a table with three rows without crashing", () => {
+        render(
+            <CoursesTable courses={coursesFixtures.threeCourses} />
+        );
+    });
+
+    test("CoursesTable is testId", async () => {
+        const {getByTestId } = render(
+            <CoursesTable courses={coursesFixtures.threeCourses} />
+        );
+        await waitFor( ()=> expect(getByTestId("CoursesTable-header-quarter")).toBeInTheDocument() );
+    });
+
+    test("renders a table with three rows with correct content", async () => {
+        const {getByTestId, getByText } = render(
+            <CoursesTable courses={coursesFixtures.threeCourses} />
+        );
+
+        const columns = [
+            {
+                accessor: "quarter",
+                Header: "Quarter"
+            },
+            {
+                accessor: "courseId",
+                Header: "Course ID"
+            },
+            {
+                accessor: "title",
+                Header: "Title"
+            },
+            {
+                accessor: "description",
+                Header: "Description"
+            },
+            {
+                accessor: "objLevelCode",
+                Header: "Course Level"
+            },
+            {
+                accessor: "subjectArea",
+                Header: "Subject Area"
+            },
+            {
+                accessor: "unitsFixed",
+                Header: "Units"
+            },
+        ]
+
+        const expectedHeaders = columns.map(column => column.Header)
+        const expectedFields = columns.map(column => column.accessor)
+        const testId = "CoursesTable";
+    
+        expectedHeaders.forEach((headerText) => {
+          const header = getByText(headerText);
+          expect(header).toBeInTheDocument();
+        });
+    
+        expectedFields.forEach((field) => {
+          const header = getByTestId(`${testId}-cell-row-0-col-${field}`);
+          expect(header).toBeInTheDocument();
+        });
+    
+        expect(getByTestId(`${testId}-cell-row-0-col-quarter`)).toHaveTextContent("20222");
+        expect(getByTestId(`${testId}-cell-row-0-col-description`)).toHaveTextContent("The nature of culture: survey of the range of cultural phenomena, including material culture, social...");
+        expect(getByTestId(`${testId}-cell-row-1-col-description`)).toHaveTextContent("yVkJXdic3rVyOLGwUY7PfdcREdO5tLi3o9QCC5nDCdb5Q01mCwqTmTVUYn5fvBjwPhj6YPxhYddOH3wr0IqpFsNq04BocVhiOwTu")
+        expect(getByTestId(`${testId}-cell-row-2-col-description`)).toHaveTextContent("ascnjasn asjcnajsndask sdjksja")
+
+    });
+
+});
