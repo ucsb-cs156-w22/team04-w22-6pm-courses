c131a3fa7b1afb721439276506420efcbd4b86e7
Joseph Armstrong <56049808+Jarmstrong845@users.noreply.github.com>
2022-03-09 17:28:08 -0800

    Update personalSchedulesFixtures.js

--- frontend/src/fixtures/personalSchedulesFixtures.js
+++ frontend/src/fixtures/personalSchedulesFixtures.js
@@ -3,26 +3,26 @@ const personalSchedulesFixtures = {
         "id": 1,
         "name": "Aragorn",
         "description": "Sword fighting in the morning, politics in the afternoon",
-        "quarter": "Spring"
+        "quarter": "20224"
     },
     threePersonalSchedules: [
         {
             "id": 1,
             "name": "Aragorn",
             "description": "Sword fighting in the morning, politics in the afternoon",
-            "quarter": "Spring"
+            "quarter": "20224"
         },
         {
             "id": 2,
             "name": "Legolas",
             "description": "Archery in the morning, underwater basket weaving in the afternoon",
-            "quarter": "Fall"
+            "quarter": "20224"
         },
         {
             "id": 3,
             "name": "Gimli",
             "description": "Mechanical engineering in the morning, nutrition in the afternoon",
-            "quarter": "Winter"
+            "quarter": "20224"
         }
     ]
 };
