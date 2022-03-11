d1534a826f8cbca266e419b8c02e218d58151dd8
NingXia <ningxia@ucsb.edu>
2022-03-11 00:11:40 -0800

    fixed imports in table component

--- frontend/src/main/components/AdminPersonalSchedules/AdminPersonalSchedulesTable.js
+++ frontend/src/main/components/AdminPersonalSchedules/AdminPersonalSchedulesTable.js
@@ -1,7 +1,7 @@
 import React from "react";
 import OurTable, { ButtonColumn } from "main/components/OurTable";
 import { useBackendMutation } from "main/utils/useBackend";
-import { cellToAxiosParamsDelete, onDeleteSuccess } from "main/utils/PersonalScheduleUtils"
+import { cellToAxiosParamsDelete, onDeleteSuccess } from "main/utils/adminPersonalScheduleUtils"
 import { useNavigate } from "react-router-dom";
 
 
