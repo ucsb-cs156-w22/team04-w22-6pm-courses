5eb6ddb7938f3439a562fe24eac84ff0537fb40b
NingXia <ningxia@ucsb.edu>
2022-03-11 14:13:26 -0800

    nx - added admin index page for personal schedules

--- frontend/src/main/pages/AdminPersonalSchedules/AdminPersonalSchedulesIndexPage.js
+++ frontend/src/main/pages/AdminPersonalSchedules/AdminPersonalSchedulesIndexPage.js
@@ -1,14 +1,29 @@
+import React from 'react'
+import { useBackend } from 'main/utils/useBackend';
+
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import AdminPersonalSchedulesTable from 'main/components/AdminPersonalSchedules/AdminPersonalSchedulesTable';
+import { useCurrentUser } from 'main/utils/currentUser'
 
 export default function AdminPersonalSchedulesIndexPage() {
+
+  const currentUser = useCurrentUser();
+
+  const { data: personalSchedules, error: _error, status: _status } =
+    useBackend(
+      // Stryker disable next-line all : don't test internal caching of React Query
+      ["/api/personalschedules/admin/all"],
+      { method: "GET", url: "/api/personalschedules/admin/all" },
+      []
+    );
+
   return (
     <BasicLayout>
       <div className="pt-2">
         <h1>All Personal Schedules</h1>
-        <p>
-          This is where the admin index page will go
-        </p>
+        <AdminPersonalSchedulesTable personalSchedules={personalSchedules} currentUser={currentUser} />
       </div>
     </BasicLayout>
   )
+  
 }
--- frontend/src/tests/pages/AdminPersonalSchedules/AdminPersonalSchedulesIndexPage.test.js
+++ frontend/src/tests/pages/AdminPersonalSchedules/AdminPersonalSchedulesIndexPage.test.js
@@ -1,21 +1,45 @@
-import { render } from "@testing-library/react";
+import { render, waitFor } from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
 import AdminPersonalSchedulesIndexPage from "main/pages/AdminPersonalSchedules/AdminPersonalSchedulesIndexPage";
 
 import { apiCurrentUserFixtures }  from "fixtures/currentUserFixtures";
 import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
+import { adminPersonalSchedulesFixtures } from "fixtures/adminPersonalSchedulesFixtures";
 import axios from "axios";
 import AxiosMockAdapter from "axios-mock-adapter";
+
+
+const mockToast = jest.fn();
+jest.mock('react-toastify', () => {
+    const originalModule = jest.requireActual('react-toastify');
+    return {
+        __esModule: true,
+        ...originalModule,
+        toast: (x) => mockToast(x)
+    };
+});
 
 describe("AdminPersonalSchedulesIndexPage tests", () => {
 
     const axiosMock =new AxiosMockAdapter(axios);
-    axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
-    axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
 
-    const queryClient = new QueryClient();
-    test("renders without crashing", () => {
+    const testId = "AdminPersonalSchedulesTable";
+
+    const setupAdminUser = () => {
+        axiosMock.reset();
+        axiosMock.resetHistory();
+        axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.adminUser);
+        axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
+    };
+
+
+
+    test("renders without crashing for admin user", () => {
+        setupAdminUser();
+        const queryClient = new QueryClient();
+        axiosMock.onGet("/api/personalschedules/admin/all").reply(200, []);
+
         render(
             <QueryClientProvider client={queryClient}>
                 <MemoryRouter>
@@ -23,8 +47,32 @@ describe("AdminPersonalSchedulesIndexPage tests", () => {
                 </MemoryRouter>
             </QueryClientProvider>
         );
+
+
     });
 
+
+
+    test("renders three dates without crashing for admin user", async () => {
+        setupAdminUser();
+        const queryClient = new QueryClient();
+        axiosMock.onGet("/api/personalschedules/admin/all").reply(200, adminPersonalSchedulesFixtures.threePersonalSchedules);
+
+        const { getByTestId } = render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <AdminPersonalSchedulesIndexPage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+
+        await waitFor(() => { expect(getByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent("1"); });
+        expect(getByTestId(`${testId}-cell-row-1-col-id`)).toHaveTextContent("2");
+        expect(getByTestId(`${testId}-cell-row-2-col-id`)).toHaveTextContent("3");
+
+    });
+
+    
 });
 
 
