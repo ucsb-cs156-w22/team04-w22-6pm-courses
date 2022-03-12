66906353faaeb5f71677f7bb4d4cd581bce4a68e
NingXia <ningxia@ucsb.edu>
2022-03-11 19:40:08 -0800

    nx - deleted commented out lines

--- frontend/src/main/pages/AdminLoadSubjectsPage.js
+++ frontend/src/main/pages/AdminLoadSubjectsPage.js
@@ -1,20 +1,4 @@
 
-// import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
-
-
-
-// const AdminLoadSubjectsPage = () => {
-
-    
-//     return (
-//         <BasicLayout>
-//             <h2>Load Subjects</h2>
-            
-//         </BasicLayout>
-//     );
-// };
-
-// export default AdminLoadSubjectsPage;
 
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 
